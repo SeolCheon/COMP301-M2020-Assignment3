b1a60fe9a2d91e1631b5689665cabbf0f194e6ef
Seol Cheon <jojo432143@gmail.com>
2020-08-06 03:26:51 -0400

    docstring added

--- Documents/workspace2/copyfile.py
+++ Documents/workspace2/copyfile.py
@@ -7,20 +7,20 @@ file description : This script will copy the contents of one file into another f
 """
 
 
-#prompt the user to enter the source file name
+'''prompt the user to enter the source file name'''
 source=input("enter the source filename with extension: ")
 
-#prompt the user to enter the destination file name 
+'''prompt the user to enter the destination file name''' 
 destination=input("enter the destination filename with extension: ")
 
-#open source file in read mode
+'''open source file in read mode'''
 source=open(source,"r")
 
-#open destination file in write mode
+'''open destination file in write mode'''
 destination=open(destination,"w+")
 
-#read the content of source file and assign that as a content
+'''read the content of source file and assign that as a content'''
 content=source.read()
 
-#write content to destination file
+'''write content to destination file'''
 destination.write(content)
--- Documents/workspace2/payroll.py
+++ Documents/workspace2/payroll.py
@@ -7,17 +7,28 @@ file description : This script will reads the data.txt file and outputs
 the wages paid to each employee in tabular format to the console
 """
 
-fileName = input("Enter file name : ")
-dataFile = open(fileName,"r")
 
+fileName = input("Enter file name : ")
+'''prompt user to answer displaying texts in input function '''
+
+dataFile = open(fileName,"r")
+'''open file that user input in console in readmode '''
 
 records = dataFile.readlines()
+'''read the lines in dataFile and assign it as records'''
+
 print("Employ Number |    Employee Name    | Hours Worked ")
+'''print these table headers'''
+
 print("---------------------------------------------")
+'''print this line'''
+
 for record in records:
     empDetail = record.split(",")
+    '''split record by comma and assign those elements in empDetail array'''
     empNumber = empDetail[0]
     empName = empDetail[1]+" "+empDetail[2]
     hourWorked = empDetail[3]
     
     print("%13s | %19s | %8s " %(empNumber,empName,hourWorked))
+    '''print data'''
