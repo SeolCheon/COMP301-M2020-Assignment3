26268a4ab0b13c37461bb124d395edd27eafdad4
Seol Cheon <jojo432143@gmail.com>
2020-08-06 02:47:16 -0400

    header comment for payroll.py added

---
+++ Documents/workspace2/payroll.py
@@ -0,0 +1,8 @@
+#HEADER COMMENT
+"""
+File name : payroll.py
+Author's name : Seol Cheon
+Student Number : 301113120
+file description : This script will reads the data.txt file and outputs
+the wages paid to each employee in tabular format to the console
+"""
