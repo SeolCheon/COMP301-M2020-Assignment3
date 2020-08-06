3ed95cbba922f74102beeabd0890e072d551b246
Seol Cheon <jojo432143@gmail.com>
2020-08-06 03:14:51 -0400

    payroll.py codes added

--- Documents/workspace2/payroll.py
+++ Documents/workspace2/payroll.py
@@ -6,3 +6,18 @@ Student Number : 301113120
 file description : This script will reads the data.txt file and outputs
 the wages paid to each employee in tabular format to the console
 """
+
+fileName = input("Enter file name : ")
+dataFile = open(fileName,"r")
+
+
+records = dataFile.readlines()
+print("Employ Number |    Employee Name    | Hours Worked ")
+print("---------------------------------------------")
+for record in records:
+    empDetail = record.split(",")
+    empNumber = empDetail[0]
+    empName = empDetail[1]+" "+empDetail[2]
+    hourWorked = empDetail[3]
+    
+    print("%13s | %19s | %8s " %(empNumber,empName,hourWorked))
