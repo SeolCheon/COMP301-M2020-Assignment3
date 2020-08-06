b0f88303596b0e0c4219ad945acf386c22670f98
Seol Cheon <jojo432143@gmail.com>
2020-08-06 02:22:38 -0400

    codes completed

--- Documents/workspace2/copyfile.py
+++ Documents/workspace2/copyfile.py
@@ -3,19 +3,9 @@
 #Student Number : 301113120
 #file description : This script will copy the contents of one file into another file
 
-#prompt the user to enter the source file name: 
 source=input("enter the source filename with extension: ")
-#prompt the user to enter the destination file name :
 destination=input("enter the destination filename with extension: ")
-#open source file in read mode
 source=open(source,"r")
-#open destination file in write mode
 destination=open(destination,"w+")
-#read the content of source file and assign that as a content
 content=source.read()
-#write content to destination file
 destination.write(content)
-
-#close source file and destination file
-source.close()
-destination.close()
